d4f3c721279725028ef9c4cde54d4708f932f938
yasin <yasinhamidi.eng@gmail.com>
2021-05-06 20:49:06 +0430

    FirstCommit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:bus_info/pages/HomePage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(body: HomePage(),),
+    );
+  }
+}
